5727a60a33bf19ca030f5ec7e4e1fb50aa1f55fa
Kaitlyn Strickfaden <kaitlyn.strickfaden@gmail.com>
2020-11-18 06:49:16 -0800

    Add progress bar
    Add progress bar. Add show_image variable to make plotting recolored images optional (saves lots of time).

--- R/ct_overlay.R
+++ R/ct_overlay.R
@@ -9,10 +9,12 @@
 #' @import dplyr
 #' @import grDevices
 #' @import imager
+#' @import progress
 #' @param images A vector containing file paths of images to be analyzed. The first image will be the reference image, and contours from the first image will be superimposed onto the other images.
 #' @param contourvalue A numeric between 0-1 for the lowest contour value you want to recolor (a low contour value captures weaker contrasts). Default is 0.1.
 #' @param color A character string for the color of the superimposed object. Default is red.
 #' @param regions A numeric indicating how many regions to draw. Default is 1.
+#' @param show_image Logical. Plot images as they are recolored. Default is TRUE.
 #' @return Recolored images. Saves recolored images to working directory with "_contourr" appended to the original name.
 #' @export
 
@@ -22,7 +24,8 @@
 ct_overlay <- function(images,
                        contourvalue = .1,
                        color = "red",
-                       regions = 1)
+                       regions = 1,
+                       show_image = TRUE)
 
 {
 
@@ -64,10 +67,11 @@ ct_overlay <- function(images,
   roi <- dplyr::distinct(roi)
 
 
-  ## Track time
+  ## Progress Bar
 
-  st <- Sys.time()
-
+  pb <- progress_bar$new(
+    format = " recoloring images  [:bar]  :percent  elapsed: :elapsed",
+    total = length(images), clear = FALSE, width = 80)
 
 
   ## Find contours in region of interest
@@ -82,6 +86,7 @@ ct_overlay <- function(images,
 
   im_df$value[im_df$id %in% m] <- rep(rgbcolor, each = length(m))
 
+  pb$tick()
 
 
   ## Display the new image
@@ -95,23 +100,19 @@ ct_overlay <- function(images,
   # Save new photo
 
   image_split1 <- stringr::str_split(images[1], "\\.")[[1]]
-
   image_end1 <- image_split1[length(image_split1)]
-
   image_name1 <- paste(image_split1[1], "_contourr.", image_end1, sep = "")
 
-
-  grDevices::jpeg(image_name1, width = dim(im)[1], height = dim(im)[2]) # begin creation of an image file
-  par(mar = c(0,0,0,0)) # remove axes and margins
-  plot(im) # plot new image
-  dev.off() # stop and save image file with contours drawn on it
-
+  grDevices::jpeg(image_name1, width = dim(im)[1], height = dim(im)[2])
+  par(mar = c(0,0,0,0))
+  plot(im)
+  dev.off()
 
 
-
-  ## Apply the "mask" to new images
+  ## Recolor other images
 
   for (i in 2:length(images)) {
+
 
     ## Load in new image
 
@@ -119,33 +120,36 @@ ct_overlay <- function(images,
     im2_df <- as.data.frame(im2)
     im2_df$id <- im_df$id
 
+
     ## Recolor contour pixels in full image
 
     im2_df$value[im2_df$id %in% m] <- rep(rgbcolor, each = length(m))
 
-    ## Display the new image
-
     im2 <- imager::as.cimg(im2_df, dim = dim(im2))
 
-    par(mar = c(0,0,0,0))
-    plot(im2)
 
+    ## (Optionally) display the new image
+
+    if (show_image == TRUE) {
+      par(mar = c(0,0,0,0))
+      plot(im2)
+      }
+
+
+    ## Save the new image
 
     image_split <- stringr::str_split(images[i], "\\.")[[1]]
-
     image_end <- image_split[length(image_split)]
-
     image_name <- paste(image_split[1], "_contourr.", image_end, sep = "")
 
-    # Save new photo
-    grDevices::jpeg(image_name, width = dim(im2)[1], height = dim(im2)[2]) # begin creation of an image file
-    par(mar = c(0,0,0,0)) # remove axes and margins
-    plot(im2) # plot new image
-    dev.off() # stop and save image file with contours drawn on it
+    grDevices::jpeg(image_name, width = dim(im2)[1], height = dim(im2)[2])
+    par(mar = c(0,0,0,0))
+    plot(im2)
+    dev.off()
 
+    pb$tick()
 
   } # End of recolor new images
 
-  Sys.time() - st
 
 } # End of function
